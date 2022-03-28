4f6872dc7177c3d72949382c7476ef351296f70b
Gustavo Ferreira <fgustavo016@gmail.com>
2022-03-28 17:42:27 -0300

    added routers and navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
 import googleIconImg from '../assets/images/google-icon.svg';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import '../styles/auth.scss';
 
-
 export function Home() {
+    const navigate = useNavigate();
+
+    function navigateToNewRoom() {
+        navigate('/rooms/new')
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -16,7 +23,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button className="create-room">
+                    <button className="create-room" onClick={() => navigateToNewRoom()}>
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
