236d9c7f68b6af68402dc629bfc522907e5cbc75
Gustavo Ferreira <fgustavo016@gmail.com>
2022-03-29 14:38:57 -0300

    join room using key

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
@@ -6,12 +7,15 @@ import illustrationImg from '../assets/images/illustration.svg';
 
 import { useAuth } from '../hooks/useAuth';
 import { Button } from '../components/Button';
+import { database } from '../services/firebase';
 
 import '../styles/auth.scss';
 
 export function Home() {
-    const navigate = useNavigate();
     const { user, signInWithGoogle } = useAuth();
+    const [ roomCode, setRoomCode ] = useState('');
+    
+    const navigate = useNavigate();
 
     async function handleCreateRoom() {
         if (!user) {
@@ -19,6 +23,22 @@ export function Home() {
         }
 
         navigate('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '')
+            return;
+        
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exist.');
+            return;
+        }
+
+        navigate(`/rooms/${roomCode}`);
     }
 
     return (
@@ -36,10 +56,12 @@ export function Home() {
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={ event => setRoomCode(event.target.value) }
+                            value={ roomCode }
                         />
                         <Button type="submit">Entrar na sala</Button>
                     </form>
